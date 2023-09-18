73d12a4b2162cb2a538fa2bbcd6a7d339e9a0e9d
Rohit <rkumar8092378845@gmail.com>
2023-09-18 15:58:31 +0530

    Initial commit 18-09-2023

---
+++ src/main/java/com/rkumar0206/mymexpenseservice/MymExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymexpenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkumar0206/mymexpenseservice/MymExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymexpenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MymExpenseServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
